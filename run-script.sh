6a2044dffa85971e7f8851b2e25c8e6a7432df29
James Harbin <james.harbin@york.ac.uk>
2020-04-21 00:55:42 +0100

    New JAR file

---
+++ run-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Launch MOOS
+cd ./middleware-java/moos-sim && ./launch.sh &
+sleep 10
+# Need to remove the GUI from middleware for automated runs
+# Supply a file name for the fault instance definition file
+java -cp jar-created/atlas-test.jar middleware.core.ATLASMain &
+java -cp jar-created/atlas-test.jar atlascollectiveintgenerator.runner.CollectiveIntRunner &
