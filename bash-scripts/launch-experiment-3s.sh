0ec3451ee7f5828ab066721d5d3d26beafb4678f
James Harbin <james.harbin@york.ac.uk>
2020-06-22 15:59:24 +0100

    New runner script

---
+++ bash-scripts/launch-experiment-3s.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-ELLA 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-GILDA 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-FRANK 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-HENRY 3.0
---
+++ bash-scripts/launch-experiment-4s.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-ELLA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-GILDA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-FRANK 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-HENRY 4.0
+
