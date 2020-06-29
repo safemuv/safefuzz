15252edfcaefeb788b85a5b30d6748b9f51ae88f
James Harbin <james.harbin@york.ac.uk>
2020-06-29 04:09:50 +0100

    Launch script

--- bash-scripts/launch-experiment.sh
+++ bash-scripts/launch-experiment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-ELLA 3.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-GILDA 3.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-FRANK 3.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-HENRY 3.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-ELLA 4.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-GILDA 4.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-FRANK 4.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-HENRY 4.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-ELLA 5.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-GILDA 5.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-FRANK 5.0
-/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunExperiment SPEEDFAULT-HENRY 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-ELLA 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-ELLA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-ELLA 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 5.0
