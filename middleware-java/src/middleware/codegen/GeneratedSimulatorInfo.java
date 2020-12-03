7ab335e08b84bac754932e9ff1e37ed29dcf6548
James Harbin <james.harbin@york.ac.uk>
2020-12-03 03:25:30 +0000

    Changes for DSL for the simulator info

---
+++ middleware-java/src/middleware/codegen/GeneratedSimulatorInfo.java
@@ -0,0 +1,8 @@
+package middleware.codegen;
+
+public class GeneratedSimulatorInfo {
+	public void getSimInfo() {
+			System.out.println("uSimMarine - DESIRED_THRUST");
+			System.out.println("uSimMarine - DESIRED_RUDDER");
+	}
+}
