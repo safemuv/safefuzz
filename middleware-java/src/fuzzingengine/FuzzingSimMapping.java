4e699d94378185d5df5026466dc829633c5fa275
James Harbin <james.harbin@york.ac.uk>
2020-12-17 03:53:10 +0000

    Changes for fuzzing EGL

--- middleware-java/src/fuzzingengine/FuzzingSimMapping.java
+++ middleware-java/src/fuzzingengine/FuzzingSimMapping.java
@@ -1,10 +1,48 @@
 package fuzzingengine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+// Stores all the information for a simulator mapping
 public class FuzzingSimMapping {
-	// Needs to store classes containing all the info from the model
+
+	public enum VariableDirection {
+		INBOUND,
+		OUTBOUND
+	}
+	
+	public class VariableSpecification {
+		private String component;
+		private String variable;
+		private String reflectionName;
+		// TODO: encode the type definition
+		private VariableDirection dir;
+		private Optional<String> binaryPath;
+		
+		public VariableSpecification(String component, String variable, String reflectionName, VariableDirection dir, Optional<String> binaryPath) {
+			this.component = component;
+			this.variable = variable;
+			this.reflectionName = reflectionName;
+			this.dir = dir;
+			this.binaryPath = binaryPath;
+		}
+	}
+	
+	// Maps the component name to variable specification
+	private HashMap<String, List<VariableSpecification>> records = new HashMap<String,List<VariableSpecification>>();
+	
+	public void addRecord(String component, String variable, String reflectionName, VariableDirection dir, Optional<String> binaryPath) {
+		if (!records.containsKey(component)) {
+			records.put(component, new ArrayList<VariableSpecification>());
+		}
+		
+		VariableSpecification vs = new VariableSpecification(component, variable, reflectionName, dir, binaryPath);
+		records.get(component).add(vs);
+	}
+	
 	
 	// Returns all the strings to mutate for a binary
 	public HashMap<String,String> getBinaryChanges(String component, HashMap<String,String> inNames) {
--- middleware-java/src/fuzzingengine/spec/GeneratedFuzzingSpec.java
+++ middleware-java/src/fuzzingengine/spec/GeneratedFuzzingSpec.java
@@ -4,22 +4,18 @@ import java.util.Optional;
 import fuzzingengine.*;
 import fuzzingengine.operations.*;
 
+
 public class GeneratedFuzzingSpec {
 	public static FuzzingEngine createFuzzingEngine() {
 	FuzzingEngine fe = new FuzzingEngine();
+	FuzzingSimMapping simMapping = new FuzzingSimMapping();
 	
-	System.out.println("uSimMarine - DESIRED_THRUST");
-	System.out.println("");
-	System.out.println("uSimMarine - DESIRED_RUDDER");
-	System.out.println("");
+
+	simMapping.addRecord("uSimMarine", "DESIRED_RUDDER", "DEZIRED_RUDDER", FuzzingSimMapping.VariableDirection.INBOUND);
+
+	simMapping.addRecord("uSimMarine", "DESIRED_THRUST", "DEZIRED_THRUST", FuzzingSimMapping.VariableDirection.INBOUND);
 	
-	FuzzingOperation nullOp = new NullFuzzingOperation();
-	FuzzingOperation thrustFuzz = NumericVariableChangeFuzzingOperation.RandomOffset(-20.0, 20.0);
-	FuzzingOperation rudderFuzz = NumericVariableChangeFuzzingOperation.RandomOffset(-50.0, 50.0);
-				
-	fe.addFuzzingOperation("DESIRED_THRUST", Optional.of("DEZIRED_THRUST"), Optional.of("uSimMarine"), thrustFuzz);
-	fe.addFuzzingOperation("DESIRED_RUDDER", Optional.of("DEZIRED_RUDDER"), Optional.of("uSimMarine"), rudderFuzz);
-	fe.addFuzzingOperation("DESIRED_ELEVATOR", Optional.of("DEZIRED_ELEVATOR"), Optional.of("uSimMarine"), nullOp);				
+	fe.setSimMapping(simMapping); 
 	return fe;
 	}
 }
