92da1c910c27b0a03b9f3fe3ce70f8cb814a60a5
James Harbin <james.harbin@york.ac.uk>
2021-07-12 02:11:59 +0100

    New launch scripts for modified SAFEMUV launchers

--- expt-working/ros/auto_launch_safemuv.sh
+++ expt-working/ros/auto_launch_safemuv.sh
@@ -1,32 +1,18 @@
 #!/bin/sh
 
-TRAJ_HOST_LOCATION=/home/jharbin/catkin_ws/src/safemuv/traj_plan_external/trajectory_planner_external.yaml
-TRAJ_DEST_LOCATION=/catkin_ws/src/planning_stack/packages/path_planning/path_planner_geometric_primitives/trajectory_planner_geometric_primitives_ros/config/path_planner_geometric_primitives_3d_airplane_cargolux_airplane_frame.yaml
-
 rm -rf /home/jharbin/.ros/log
 
-# TODO: reduce the time delays
-echo "Starting SAFEMUV Docker containers"
-# To setup the images for the first run
-#docker run --name SAFEMUV1 -it --network host -v /home/jharbin/catkin_ws/src/safemuv/safemuv_shared:/catkin_ws/src/safemuv_shared afi_base_software_multi bash
-#docker run --name SAFEMUV2 -it --network host -v /home/jharbin/catkin_ws/src/safemuv/safemuv_shared:/catkin_ws/src/safemuv_shared afi_base_software_multi bash
-docker start SAFEMUV1
-docker start SAFEMUV2
+echo "Starting SAFEMUV Docker container"
+docker run -it --network host -v /home/$USER/catkin_ws/src/safemuv_ros/safemuv_shared/:/catkin_ws/src/safemuv_shared afi_core bash
 sleep 10
-#docker cp $TRAJ_HOST_LOCATION SAFEMUV1:$TRAJ_DEST_LOCATION
-#docker cp $TRAJ_HOST_LOCATION SAFEMUV2:$TRAJ_DEST_LOCATION
-sleep 1
 echo "Starting roslaunch launchers"
 xterm -e /bin/bash -l -c "roslaunch safemuv_gazebo_simulation prepare_sim.launch" &
 sleep 5
-xterm -e /bin/bash -l -c "roslaunch safemuv_launchers safemuv_launch_obstacle_publisher.launch" &
+xterm -e /bin/bash -l -c "docker exec -it SAFEMUV1 bash -c 'source /catkin_ws/devel/setup.bash && roslaunch safemuv_shared multi_afi_bringup.launch'" &
 sleep 5
 xterm -e /bin/bash -l -c "roslaunch safemuv_launchers safemuv_visualizator.launch" &
-xterm -e /bin/bash -l -c "docker exec -it SAFEMUV1 bash -c 'source /catkin_ws/devel/setup.bash && roslaunch safemuv_shared afi_uav_1.launch'" &
-xterm -e /bin/bash -l -c "docker exec -it SAFEMUV2 bash -c 'source /catkin_ws/devel/setup.bash && roslaunch safemuv_shared afi_uav_2.launch'" &
 sleep 10
-xterm -e /bin/bash -l -c "rosrun safemuv_mission state_machine.py" &
-xterm -e /bin/bash -l -c "rosrun safemuv_mission state_machine_2.py"
+xterm -e /bin/bash -l -c "roslaunch safemuv_mission multi_mission.launch" &
 
 # in the new version, only launch 1 container
 # Pedro's new version
