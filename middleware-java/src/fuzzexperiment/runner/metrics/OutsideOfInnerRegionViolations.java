24d617998c80180e791a98bce4bdbeaa875f196a
James Harbin <james.harbin@york.ac.uk>
2021-08-26 04:28:21 +0100

    InnerRegionViolations metric

--- middleware-java/src/fuzzexperiment/runner/metrics/OutsideOfInnerRegionViolations.java
+++ middleware-java/src/fuzzexperiment/runner/metrics/OutsideOfInnerRegionViolations.java
@@ -1,6 +1,10 @@
 // protected region customHeaders on begin
 package fuzzexperiment.runner.metrics;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
+
 import fuzzingengine.FuzzingKeySelectionRecord;
 // protected region customHeaders end
 
@@ -8,7 +12,34 @@ public class OutsideOfInnerRegionViolations extends OfflineMetric {
 	public Double computeFromLogs(List<FuzzingKeySelectionRecord> recs, String logDir) throws MetricComputeFailure {
 		// Implement the metric here
 		// protected region userCode on begin
-		return 0.0;
+		int outsideRegionViolations = 0;
+		String filename = logDir + "/goalLog.log";
+		System.out.println("OutsideOfOuterRegionViolations filename = " + filename);
+		Scanner reader;
+		try {
+			reader = new Scanner(new File(filename));
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
+				String[] fields = line.split(",");
+				String goalClass = fields[0];
+
+				//System.out.println("goalClass = " + goalClass);
+				if (goalClass.equals("atlasdsl.MaintainDistanceFrom")) {
+					int count = Integer.parseInt(fields[1]);
+					outsideRegionViolations = Math.max(count, outsideRegionViolations);
+				}
+			}
+
+			reader.close();
+			System.out.println("outsideRegionViolations = " + outsideRegionViolations);
+						
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return Double.valueOf(outsideRegionViolations);	
 		// protected region userCode end
 	}
 }
