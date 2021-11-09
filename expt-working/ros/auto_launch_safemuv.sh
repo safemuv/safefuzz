bf96ed49f66812d092873917602c2df6bece21ff
James Harbin <james.harbin@york.ac.uk>
2021-11-09 03:33:48 +0000

    Changes to the experiment metric for speed violations

--- expt-working/ros/auto_launch_safemuv.sh
+++ expt-working/ros/auto_launch_safemuv.sh
@@ -7,7 +7,7 @@ xterm -e /bin/bash -l -c "roslaunch safemuv_gazebo_simulation prepare_sim.launch
 sleep 25
 echo "Starting SAFEMUV Docker container"
 xterm -e docker run --network host -v /home/$USER/catkin_ws/src/safemuv_ros/safemuv_shared/:/catkin_ws/src/safemuv_shared afi_core /bin/bash -c 'source /catkin_ws/devel/setup.bash && roslaunch safemuv_shared multi_afi_bringup.launch' &
-xterm -e docker run -it --network host safemuv_metrics &
+xterm -e docker run -it --network host safemuv_metrics_3s &
 sleep 15
 echo "Starting roslaunch launchers"
 xterm -e /bin/bash -l -c "roslaunch safemuv_rviz_visualization safemuv_simulation_visualization.launch" &
