63f6e7333974e3fba8025ea115fc1f472d69fd16
James Harbin <james.harbin@york.ac.uk>
2022-01-04 22:24:49 +0000

    Changes for the rkmg graph caller

---
+++ expt-working/ros/generate_launch_files_rmkg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCENARIO_ID=$1
+TEST_NUM=$2
+FUZZ_TOPICS=$3
+FUZZ_CONFIG=$4
+CONFIG_DIR=$5
+
+. ~/academic/safemuv/knowledge_graph/safemuv/bin/activate
+cd ~/catkin_ws/src/scenario_generation
+
+if [ -z "${FUZZ_TOPICS}" ]; then
+	STR="python3 ./scenario_generation/scripts/rmkg_ros $SCENARIO_ID --test-id $TEST_NUM --fuzzing-config $FUZZ_CONFIG --config-files $CONFIG_DIR"
+else
+	STR="python3 ./scenario_generation/scripts/rmkg_ros $SCENARIO_ID --test-id $TEST_NUM --fuzzing-config $FUZZ_CONFIG --fuzz-topics $FUZZ_TOPICS --config-files $CONFIG_DIR"
+fi
+echo $STR > /tmp/scen_cmd
+eval $STR
