14649835bf2505666cd930ef95f7acd9929c85fe
James Harbin <james.harbin@york.ac.uk>
2022-03-02 02:39:50 +0000

    Changes to launchers

--- expt-working/ros/custom_auto_launch_safemuv_exp_lab.sh
+++ expt-working/ros/custom_auto_launch_safemuv_exp_lab.sh
@@ -6,7 +6,9 @@ SCEN_LAUNCH_BASE="$HOME/catkin_ws/src/scenario_generation/safemuv_scenarios/scen
 
 echo "Starting roslaunch launchers"
 xterm -e /bin/bash -l -c "source ~/catkin_ws/devel/setup.bash && roslaunch $SCEN_LAUNCH_BASE/$1/launch/$1_lab.launch" &
-sleep 15
+sleep 5
+xterm -e /bin/bash -l -c "source ~/catkin_ws/devel/setup.bash && roslaunch safemuv_gazebo_simulation rosbridge.launch" &
+sleep 10
 echo "Starting SAFEMUV Docker container"
 xterm -e docker run --network host -v /home/$USER/catkin_ws/src/safemuv_ros/safemuv_shared/:/catkin_ws/src/safemuv_shared afi_core /bin/bash -c "source /catkin_ws/devel/setup.bash && roslaunch safemuv_shared $1_afi_bringup.launch" &
 xterm -e docker run -it --network host metrics_test_2 &
